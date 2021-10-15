faa84cc49794449196772e2f3f9066504b200c42
karl <karl.bittner@hexaquo.at>
2021-10-15 11:07:52 +0200

    Autoformat and delete move/copy ctor/assignment operator

--- main_skeleton.cpp
+++ main_skeleton.cpp
@@ -1,107 +1,128 @@
 /**
-* Exercise: "DoubleEndedStackAllocator with Canaries" OR "Growing DoubleEndedStackAllocator with Canaries (VMEM)"
-* Group members: Amon Shokhin Ahmed (gs20m014), Karl Bittner (gs20m013), David Panagiotopulos (gs20m019)
-**/
+ * Exercise: "DoubleEndedStackAllocator with Canaries" OR "Growing DoubleEndedStackAllocator with Canaries (VMEM)"
+ * Group members: Amon Shokhin Ahmed (gs20m014), Karl Bittner (gs20m013), David Panagiotopulos (gs20m019)
+ **/
 
 #include "stdio.h"
 
 namespace Tests
 {
-	void Test_Case_Success(const char* name, bool passed)
-	{
-		if (passed)
-		{
-			printf("[%s] passed the test!\n", name);
-		}
-		else
-		{
-			printf("[%s] failed the test!\n", name);
-		}
-	}
+    void Test_Case_Success(const char *name, bool passed)
+    {
+        if (passed)
+        {
+            printf("[%s] passed the test!\n", name);
+        }
+        else
+        {
+            printf("[%s] failed the test!\n", name);
+        }
+    }
 
-	void Test_Case_Failure(const char* name, bool passed)
-	{	
-		if (!passed)
-		{
-			printf("[%s] passed the test!\n", name);
-		}
-		else
-		{
-			printf("[%s] failed the test!\n", name);
-		}
-	}
+    void Test_Case_Failure(const char *name, bool passed)
+    {
+        if (!passed)
+        {
+            printf("[%s] passed the test!\n", name);
+        }
+        else
+        {
+            printf("[%s] failed the test!\n", name);
+        }
+    }
 
-	/** 
-	* Example of how a test case can look like. The test cases in the end will check for
-	* allocation success, proper alignment, overlaps and similar situations. This is an
-	* example so you can already try to cover all cases you judge as being important by
-	* yourselves.
-	**/
-	template<class A>
-	bool VerifyAllocationSuccess(A& allocator, size_t size, size_t alignment)
-	{
-		void* mem = allocator.Allocate(size, alignment);
+    /**
+     * Example of how a test case can look like. The test cases in the end will check for
+     * allocation success, proper alignment, overlaps and similar situations. This is an
+     * example so you can already try to cover all cases you judge as being important by
+     * yourselves.
+     **/
+    template <class A> bool VerifyAllocationSuccess(A &allocator, size_t size, size_t alignment)
+    {
+        void *mem = allocator.Allocate(size, alignment);
 
-		if (mem == nullptr)
-		{
-			printf("[Error]: Allocator returned nullptr!\n");
-			return false;
-		}
+        if (mem == nullptr)
+        {
+            printf("[Error]: Allocator returned nullptr!\n");
+            return false;
+        }
 
-		return true;
-	}
-}
+        return true;
+    }
+} // namespace Tests
 
-// Assignment functionality tests are going to be included here 
+// Assignment functionality tests are going to be included here
 
 // If set to 1, Free() and FreeBack() should assert if the memory canaries are corrupted
 #define WITH_DEBUG_CANARIES 0
 
 /**
-* You work on your DoubleEndedStackAllocator. Stick to the provided interface, this is
-* necessary for testing your assignment in the end. Don't remove or rename the public
-* interface of the allocator. Also don't add any additional initialization code, the
-* allocator needs to work after it was created and its constructor was called. You can
-* add additional public functions but those should only be used for your own testing.
-**/
+ * You work on your DoubleEndedStackAllocator. Stick to the provided interface, this is
+ * necessary for testing your assignment in the end. Don't remove or rename the public
+ * interface of the allocator. Also don't add any additional initialization code, the
+ * allocator needs to work after it was created and its constructor was called. You can
+ * add additional public functions but those should only be used for your own testing.
+ **/
 class DoubleEndedStackAllocator
 {
-public:
-	DoubleEndedStackAllocator(size_t max_size) {}
+  public:
+    DoubleEndedStackAllocator(size_t max_size)
+    {
+    }
+    ~DoubleEndedStackAllocator(void)
+    {
+    }
 
-	// TODO: Add Copy and Move Constructor / Assignment Operator (delete them?)
+    // Copy and Move Constructors / Assignment Operators are explicitly deleted.
+    // There are some ways to get them to work, but they all require the user to be very cautious and might be
+    // unexpected (especially when called implicitly), so we think it's best to keep the user from invoking them
+    // entirely.
+    DoubleEndedStackAllocator(const DoubleEndedStackAllocator &other) = delete;             // Copy Constructor
+    DoubleEndedStackAllocator(DoubleEndedStackAllocator &&other) = delete;                  // Move Constructor
+    DoubleEndedStackAllocator &operator=(const DoubleEndedStackAllocator &other) = delete;  // Copy Assignment Operator
+    DoubleEndedStackAllocator &operator=(const DoubleEndedStackAllocator &&other) = delete; // Move Assignment Operator
 
-	// Alignment must be a power of two.
-	// Returns a nullptr if there is not enough memory left.
-	void* Allocate(size_t size, size_t alignment) { return nullptr; }
-	void* AllocateBack(size_t size, size_t alignment) { return nullptr; }
+    // Alignment must be a power of two.
+    // Returns a nullptr if there is not enough memory left.
+    void *Allocate(size_t size, size_t alignment)
+    {
+        return nullptr;
+    }
+    void *AllocateBack(size_t size, size_t alignment)
+    {
+        return nullptr;
+    }
 
-	// LIFO is assumed.
-	void Free(void* memory) {}
-	void FreeBack(void* memory) {}
+    // LIFO is assumed.
+    void Free(void *memory)
+    {
+    }
+    void FreeBack(void *memory)
+    {
+    }
 
-	// Clear the internal state so that the whole allocator range is available again.
-	void Reset(void) {}
+    // Clear the internal state so that the whole allocator range is available again.
+    void Reset(void)
+    {
+    }
 
-	~DoubleEndedStackAllocator(void) {}
-
-private:
+  private:
 };
-
 
 int main()
 {
-	// You can add your own tests here, I will call my tests at then end with a fresh instance of your allocator and a specific max_size
-	{
-		// You can remove this, just showcasing how the test functions can be used
-		DoubleEndedStackAllocator allocator(1024u);
-		Tests::Test_Case_Success("Allocate() returns nullptr", [&allocator](){ return allocator.Allocate(32, 1) == nullptr; }());
-	}
+    // You can add your own tests here, I will call my tests at then end with a fresh instance of your allocator and a
+    // specific max_size
+    {
+        // You can remove this, just showcasing how the test functions can be used
+        DoubleEndedStackAllocator allocator(1024u);
+        Tests::Test_Case_Success("Allocate() returns nullptr",
+                                 [&allocator]() { return allocator.Allocate(32, 1) == nullptr; }());
+    }
 
-	// You can do whatever you want here in the main function
+    // You can do whatever you want here in the main function
 
-	// Here the assignment tests will happen - it will test basic allocator functionality. 
-	{
-
-	}
+    // Here the assignment tests will happen - it will test basic allocator functionality.
+    {
+    }
 }
